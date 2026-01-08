900d48edb72570383ab16a78155a742b3c3da8f5
Lil <121698535+chasx92@users.noreply.github.com>
2026-01-08 04:38:38 +0100

    Update home separators and footer background

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,32 +6,16 @@ import { Benefits } from '../components/Benefits';
 import { Pricing } from '../components/Pricing';
 import { FAQ } from '../components/FAQ';
 import { LeadForm } from '../components/LeadForm';
-import { SectionDivider } from '../components/SectionDivider';
 
 export function Home() {
     return (
         <>
             <Hero />
             <ProofBar />
-            <div className="hidden md:block py-10">
-                <SectionDivider variant="blue" />
-            </div>
             <Steps />
-            <div className="hidden md:block py-10">
-                <SectionDivider variant="blue" />
-            </div>
             <Benefits />
-            <div className="hidden md:block py-10">
-                <SectionDivider variant="blue" />
-            </div>
             <Pricing />
-            <div className="hidden md:block py-10">
-                <SectionDivider variant="blue" />
-            </div>
             <FAQ />
-            <div className="hidden md:block py-10">
-                <SectionDivider variant="blue" />
-            </div>
             <LeadForm />
         </>
     );
